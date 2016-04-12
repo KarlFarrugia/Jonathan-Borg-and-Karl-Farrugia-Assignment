b364df3db628c0f718f7c73332c15a31672af695
Jonathan Borg <jonathan.borg.14@um.edu.mt>
2016-04-12 11:51:38 +0200

    Initial decleration of the files and classes

---
+++ AssignmentPart1/src/main/java/com/mycompany/assignmentpart1/Book.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assignmentpart1;
+
+import java.util.*;
+/**
+ *
+ * @author Jonathan Borg and Karl Farrugia
+ */
+public class Book {
+    private String title;
+    private String author;
+    private Genre genre;
+    private Date dateOfPublish;
+    private int edition;
+    private Date dateOfLoan;
+    private User loanTo;
+       
+}
+
+
---
+++ AssignmentPart1/src/main/java/com/mycompany/assignmentpart1/Catalogue.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assignmentpart1;
+
+/**
+ * Methods to implement:
+ * getAllBooke()
+ * 
+ */
+
+
+/**
+ *
+ * @author Jonathan Borg and Karl Farrugia
+ */
+public class Catalogue {
+    
+    
+}
