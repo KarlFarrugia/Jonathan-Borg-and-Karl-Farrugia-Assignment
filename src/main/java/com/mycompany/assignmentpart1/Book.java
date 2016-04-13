ede8fc463d86f9400ebe74fc08da80eeb02f8354
Karl Farrugia <karlfarrugia@live.com>
2016-04-13 10:21:02 +0200

    change pom

--- src/main/java/com/mycompany/assignmentpart1/Book.java
+++ src/main/java/com/mycompany/assignmentpart1/Book.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.assignmentpart1;
 
 import java.util.*;
@@ -29,5 +24,3 @@ public class Book {
         this.title = title;
     }
 }
-
-
--- src/main/java/com/mycompany/assignmentpart1/User.java
+++ src/main/java/com/mycompany/assignmentpart1/User.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package com.mycompany.assignmentpart1;
+
 import java.util.Date;
+
 /**
  *
  * @author jonathan
